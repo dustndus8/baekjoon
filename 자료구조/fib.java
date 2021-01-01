88307f4ab56a029ef412e6529cf73e6668b9ae26
YeonSooYeon <dustndus8@naver.com>
2021-01-01 21:29:47 +0900

    feat : fib

---
+++ 자료구조/fib.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int fib(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return (fib(n-1)+fib(n-2));
+    }
+
+    public static void main(String[] args){
+
+        int input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextInt();
+
+        System.out.print(fib(input));
+    }
+}
